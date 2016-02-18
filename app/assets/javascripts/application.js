968816223a1fa88c600f9772fe73c90036353f9e
sebtn <tonomudo@gmail.com>
2016-02-17 23:28:01 -0600

    Check size on Svg

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,20 @@
 //= require_tree .
 //= require jquery-ui
 //= require bootstrap-sprockets
+
+// $(window).load(function() {       
+// 	var theWindow = $(window),
+// 	    $bg = $("#bg"),
+// 	    aspectRatio = $bg.width() / $bg.height();    			    		
+// 	function resizeBg() {		
+// 		if ((theWindow.width() / theWindow.height()) < aspectRatio ) {
+// 		    $bg
+// 		    	.removeClass()
+// 		    	.addClass('bgheight');
+// 		} else {
+// 		    $bg
+// 		    	.removeClass()
+// 		    	.addClass('bgwidth');
+// 		}				
+// 	}                   			
+// 	theWindow.resize(resizeBg).trigger("resize");
