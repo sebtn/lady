5e3feb1d0082a7a15e452a2075d8f9a43f84347a
sebtn <tonomudo@gmail.com>
2016-02-18 02:05:57 -0600

    Check size on Svg

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,11 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
-//
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
-//
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
 //= require jquery
 //= require bootstrap-sprockets
 //= require jquery_ujs
@@ -17,19 +13,3 @@
 //= require jquery-ui
 //= require bootstrap-sprockets
 
-// $(window).load(function() {       
-// 	var theWindow = $(window),
-// 	    $bg = $("#bg"),
-// 	    aspectRatio = $bg.width() / $bg.height();    			    		
-// 	function resizeBg() {		
-// 		if ((theWindow.width() / theWindow.height()) < aspectRatio ) {
-// 		    $bg
-// 		    	.removeClass()
-// 		    	.addClass('bgheight');
-// 		} else {
-// 		    $bg
-// 		    	.removeClass()
-// 		    	.addClass('bgwidth');
-// 		}				
-// 	}                   			
-// 	theWindow.resize(resizeBg).trigger("resize");
